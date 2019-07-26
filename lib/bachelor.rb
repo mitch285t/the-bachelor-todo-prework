9c9f4f0b15a1ce7c8ebe3a32a936b4108b176461
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 19:11:37 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -30,7 +30,8 @@ end
 
 def get_occupation(data, hometown)
   data.each do |season, contestant|
-    
+    contestant.each do |contestant|
+      if contestant|"hometown"| == hometown
 end
 
 def get_average_age_for_season(data, season)
