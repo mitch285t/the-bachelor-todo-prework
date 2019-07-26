afd32a4d1babcfc6356aff6683040004d2c921e0
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 19:09:29 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -29,7 +29,8 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant|
+    
 end
 
 def get_average_age_for_season(data, season)
