4fbbc84206ad8509da05a6c49b5941ee9e18266f
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 19:20:20 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -44,6 +44,8 @@ def get_average_age_for_season(data, season)
   data[season].each do |contestant|
     contestant.each do |key, value|
       if key == "age"
-        count += 1 
+        contestant_number += 1 
         age = value.to_f
+      end 
+    end 
         
