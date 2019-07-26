7220184aabb626d02c39918fe4448a718b485112
mitchell williams <mscottwilliams1994@gmail.com>
2019-07-26 19:25:06 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,54 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
-end
-
-def get_contestant_name(data, occupation)
-  # code here
-end
-
+  data[season].each do |person_hash|
+   if person_hash["status"] == "Winner"
+     return person_hash["name"].split()[0]
+   end
+ end
+ end
+  def get_contestant_name(data, occupation)  
+  data.each do |season, person_hash|
+    person_hash.each do |person_hash|
+      if person_hash["occupation"] == occupation 
+        return person_hash["name"]
+      end 
+    end 
+  end 
+      
+end 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+      i = 0 
+      data.each do |season, contestant|
+        contestant.each do |contestant|
+          if contestant["hometown"] == hometown 
+            i += 1
+          end 
+        end 
+      end 
+      i 
+        
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end 
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
-end
+ total_age = 0 
+ contestant_num = 0 
+ 
+ data[season].each do |person_hash|
+   total_age += person_hash["age"].to_i
+   contestant_num += 1 
+ end 
+ average = (total_age/contestant_num.to_f).round
+ 
+ end
+ 
+ 
+        
